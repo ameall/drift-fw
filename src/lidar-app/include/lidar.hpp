ab0a8c7eca872f053f27ee818016cc1f2603d901
ameall <a01.meall@gmail.com>
2025-02-23 01:23:54 -0500

    Issue 13: Make Lidar App
    Minor updates to documentation and the such.

--- src/lidar-app/include/lidar.hpp
+++ src/lidar-app/include/lidar.hpp
@@ -5,12 +5,10 @@
 
 #pragma once
 
-// @cond
 #include <cstdint>
 #include <string>
-// @endcond
 
-constexpr uint32_t BAUD_RATE = 115200u;
+constexpr int8_t INVALID_SERIAL_PORT = -1;
 constexpr uint8_t LIDAR_PACKAGE_SIZE = 9u;
 
 class Lidar {
@@ -18,16 +16,34 @@ class Lidar {
     Lidar(const std::string &port);
     ~Lidar() = default;
 
+    /**
+     * @brief Opens the serial port specified by port_name
+     *
+     * @return true if the serial port is successfully opened and configured
+     * @return false if the serial port is unable to be connected to/configured
+     */
     bool open_serial();
 
+    /**
+     * @brief Gets a distance reading from the LiDAR sensor
+     *
+     * @return The distance reading in cm, or 0 if the distance cannot be read
+     */
     uint16_t get_distance();
 
+    /**
+     * @brief Closes the serial port associated with the LiDAR sensor
+     */
     void close_serial();
 
   private:
-    int32_t serial_port = -1;
+    int32_t serial_port = INVALID_SERIAL_PORT;
 
     std::string port_name = "";
 
+    /**
+     * @brief LiDAR reading; for a breakdown of the package contents refer to:
+     *      https://cdn.sparkfun.com/assets/2/b/0/3/8/TFmini_Plus-01-A02-Datasheet_EN.pdf
+     */
     uint8_t lidar_reading[LIDAR_PACKAGE_SIZE];
 };
